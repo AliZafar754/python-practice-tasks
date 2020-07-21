713feaf420a9b18105bb6eec4c91b2b38373fe97
Ali zafar <55836355+AliZafar754@users.noreply.github.com>
2020-07-21 20:48:53 +0500

    update python task 10

--- pt10.py
+++ pt10.py
@@ -2,15 +2,21 @@ class Person(object):
     def getGender( self ):
         return "Unknown"
 
+
 class Male( Person ):
     def getGender( self ):
         return "Male"
+
+
+aMale = Male()
+print(aMale.getGender())
+
 
 class Female( Person ):
     def getGender( self ):
         return "Female"
 
-aMale = Male()
-aFemale= Female()
-print(aMale.getGender())
+
+aFemale = Female()
 print(aFemale.getGender())
+
