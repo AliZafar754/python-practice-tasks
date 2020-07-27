5d384b7e9828147490238f079136abcc1b1697a4
Ali zafar <55836355+AliZafar754@users.noreply.github.com>
2020-07-28 00:58:01 +0500

    task 13

---
+++ pt13.py
@@ -0,0 +1,22 @@
+from functools import wraps
+from time import time
+
+
+def timing(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = f(*args, **kwargs)
+        end = time()
+        print('Elapsed time: {}'.format(end-start))
+        return result
+    return wrapper
+
+
+@timing
+def f(a):
+    for _ in range(a):
+        pass
+
+
+print(f(2000000))
